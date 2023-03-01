3b1f87cea91a9d3f0cefc4b2dbeaafa6a8e67a02
Zach Spelce <zbspelce@gmail.com>
2023-03-01 10:44:01 -0600

    isError - tests code output if it is an error

--- Fram Game/Editor/Editor.gd
+++ Fram Game/Editor/Editor.gd
@@ -12,6 +12,38 @@ func _ready():
 #func _process(delta):
 #	pass
 
+# parse a string from the OS.execute command and check if it is an error message
+func isError(code_message):
+	
+	# Errors we have a description for
+	var errors = [
+		"TypeError",
+		"SyntaxError"
+	]
+	
+	# Create a regex
+	var regex = RegEx.new()
+	
+	# Check for the word "File", then match any characters until the phrase ", line"
+	regex.compile("File.*, line")
+	var result = regex.search(code_message)
+	
+	var errored = ( result != null )
+	var type = null
+	
+	# If the result is not null, then an error was found
+	if errored:
+	
+		for error in errors:
+			# Check if error is in the message
+			regex.compile(error)
+			result = regex.search(code_message)
+			if result:
+				type = error
+				break
+		
+	return [errored, type]
+
 func _on_Button_pressed():
 	var code_text = get_text()
 	var python_dir = "./python_files/python.exe"
@@ -30,5 +62,9 @@ func _on_Button_pressed():
 	var exit_code = OS.execute(python_dir, [code_path], true, stdout, true)
 	print(stdout, " \nexit_code: ", exit_code)
 	
+	var code_output = stdout[0]
+	
+	print("isError: ", isError(code_output))
+	
 	# Change output box to the result of Python code
-	output.text = stdout[0]
+	output.text = code_output

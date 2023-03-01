a5768291f5090c37d5abeace87e41e37b985f9a0
Ethan Martin <emartin@tamu.edu>
2023-03-01 10:20:44 -0600

    json basic format with parsing

--- Fram Game/Puzzle/puzzleTerminal.gd
+++ Fram Game/Puzzle/puzzleTerminal.gd
@@ -6,10 +6,21 @@ extends Node2D
 # var b = "text"
 
 
+
+
+	
+func checkWork():
+	var file = File.new()
+	file.open("res://Puzzle/TestCases/test.json", file.READ)
+	var jsonText = file.get_as_text()
+	var testCases = JSON.parse(jsonText).result
+	var cases = testCases["cases"]
+	
+	
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	checkWork() # Replace with function body.
+	#TODO: somehow call _on_Button_pressed() with the parameters of input, and get the output
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):

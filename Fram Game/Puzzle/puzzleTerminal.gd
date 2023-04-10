3b8747b700d60e0ce4afee69cc7bc8fb960c8a2f
Zach Spelce <zbspelce@gmail.com>
2023-04-09 20:37:16 -0500

    prompt system

--- Fram Game/Puzzle/puzzleTerminal.gd
+++ Fram Game/Puzzle/puzzleTerminal.gd
@@ -63,6 +63,12 @@ func _ready():
 	var readOnlyLines = JSON.parse(sourceData.get_as_text()).result["readOnly"]
 	$Editor.get_node("VBoxContainer").get_node("Input").readonly_set(readOnlyLines)
 	
+	# fetch prompt and set the text
+	var promptText = File.new()
+	promptText.open("res://SourceFiles/" + source_path + "Prompt.txt", File.READ)
+	$Prompt.text = promptText.get_as_text()
+	promptText.close()
+	
 	# _ready copies the test script from PythonScripts to user://
 	var file = File.new()
 	file.open("res://PythonScripts/testCode.py", File.READ)
